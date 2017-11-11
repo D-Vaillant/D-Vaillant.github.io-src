311eda6cdb6c4175ccd9b3181f86cf703a58b5d4
D-Vaillant <dvaillant@gmail.com>
2017-11-11 13:19:38 -0800

    Hmm.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,8 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://vaillant.io'
+SITEURL = 'd-vaillant.github.io'
+# SITEURL = 'http://vaillant.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'

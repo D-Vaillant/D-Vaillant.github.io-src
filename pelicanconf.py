8c0c7909f93df224c4e9a0ad0f080e4fadd3d09d
D-Vaillant <dvaillant@gmail.com>
2020-10-25 21:07:48 -0700

    Added more stuff. Depends on more now.

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'D. Vaillant'
+import bulrush
+
+
+AUTHOR = "David O'Dwyer"
 SITENAME = 'vaillant.io'
 SITETITLE = 'vaillant.io'
-SITESUBTITLE = 'A sequence of writings by D. Vaillant.'
 SITEURL = ''
+SITESUBTITLE = 'A sequence of writings.'
 SITELOGO = SITEURL + '/images/profile.png'
 # FAVICON = SITEURL + '/images/favicon.ico'
 
@@ -17,12 +20,17 @@ CC_LICENSE = {
         'slug': 'by-sa'
 }
 
-COPYRIGHT_YEAR = 2018
+COPYRIGHT_YEAR = 2020
 
-SITEURL = 'D-Vaillant.github.io'  # can do redirect to vaillant.io
+#SITEURL = 'D-Vaillant.github.io'  # can do redirect to vaillant.io
+# SITEURL = 'vaillant.io'
 
 PATH = 'content'
-THEME = 'themes/Flex'
+
+THEME = bulrush.PATH
+JINJA_ENVIRONMENT = bulrush.ENVIRONMENT
+JINJA_FILTERS = bulrush.FILTERS
+
 READERS = {'html': None}
 
 TIMEZONE = 'America/Los_Angeles'
@@ -46,3 +54,12 @@ DEFAULT_PAGINATION = 7
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['assets']
+
+PATH_METADATA = ".*/(?P<title>.*).md"
+
+STATIC_PATHS = ['extra']
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'custom.css'},
+}
